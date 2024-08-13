a197cfb9c05256271445ccf1174a80f09cd56e05
AlejandroAldana99 <baker-plan-b@hotmail.com>
2024-08-12 22:14:20 -0600

    Update models

--- models/interactions.go
+++ models/interactions.go
@@ -2,10 +2,14 @@ package models
 
 import "time"
 
+//go:generate go-enum --marshal --
+// ENUM(view, click, add_to_cart)
+type ActionType int
+
 type UserInteraction struct {
-	UserID     string    `json:"user_id"`
-	ProductSKU string    `json:"product_sku"`
-	Action     string    `json:"action"`
-	Timestamp  time.Time `json:"timestamp"`
-	Duration   int       `json:"duration,omitempty"`
+	UserID     string     `json:"user_id"`
+	ProductSKU string     `json:"product_sku"`
+	Action     ActionType `json:"action"`
+	Timestamp  time.Time  `json:"timestamp"`
+	Duration   int        `json:"duration,omitempty"`
 }
