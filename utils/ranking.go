654c8cae29d6f167694d8951b6d894fb8c518032
AlejandroAldana99 <baker-plan-b@hotmail.com>
2024-08-12 22:13:58 -0600

    Update ranking.go

--- utils/ranking.go
+++ utils/ranking.go
@@ -1,25 +1,35 @@
 package utils
 
-import "sort"
+import (
+	"sort"
 
-func RankTopProducts(productRanks map[string]int) []string {
+	"github.com/AlejandroAldana99/yalo-challenge/models"
+)
+
+func RankTopProducts(interactions []models.UserInteraction) []string {
+	ranks := make(map[string]int)
+
+	for _, interaction := range interactions {
+		ranks[interaction.ProductSKU] += interaction.Duration
+	}
+
 	type productRank struct {
 		ProductSKU string
 		Count      int
 	}
 
-	ranks := make([]productRank, 0, len(productRanks))
-	for productSKU, count := range productRanks {
-		ranks = append(ranks, productRank{ProductSKU: productSKU, Count: count})
+	ranksList := make([]productRank, 0, len(ranks))
+	for sku, count := range ranks {
+		ranksList = append(ranksList, productRank{ProductSKU: sku, Count: count})
 	}
 
-	sort.Slice(ranks, func(i, j int) bool {
-		return ranks[i].Count > ranks[j].Count
+	sort.Slice(ranksList, func(i, j int) bool {
+		return ranksList[i].Count > ranksList[j].Count
 	})
 
-	topProducts := []string{}
-	for i := 0; i < len(ranks) && i < 3; i++ {
-		topProducts = append(topProducts, ranks[i].ProductSKU)
+	topProducts := make([]string, 0, 3)
+	for i := 0; i < len(ranksList) && i < 3; i++ {
+		topProducts = append(topProducts, ranksList[i].ProductSKU)
 	}
 
 	return topProducts
